b08cef3df4ab24e2b8839acca6f2f97ba0b1a9f1
SirCarlosPedraza <59781461+CarlosPedraza456@users.noreply.github.com>
2023-06-25 23:42:28 -0400

    Cambios en base de datos
    Cree una nueva migracion para almacenar los registros de los clientes registrados y a mostrar.

--- farmaciaDSA/Farmacia/admin.py
+++ farmaciaDSA/Farmacia/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from Farmacia.models import product
+from Farmacia.models import clientes
 
 admin.site.register(product)
+admin.site.register(clientes)
--- farmaciaDSA/Farmacia/models.py
+++ farmaciaDSA/Farmacia/models.py
@@ -7,3 +7,10 @@ class product(models.Model):
     product_price = models.IntegerField()
     product_lot = models.IntegerField()
 
+class clientes(models.Model):
+    cliente_name = models.CharField(max_length=35)
+    cliente_apellido = models.CharField(max_length=35)
+    cliente_cedula = models.CharField(max_length=15)
+    cliente_edad = models.IntegerField()
+    cliente_fecha = models.DateField()
+    
