5f796939f345485632e2d5283c085f4076573dd6
SirCarlosPedraza <59781461+CarlosPedraza456@users.noreply.github.com>
2023-07-24 21:38:17 -0400

    Adding and listing clients

--- farmaciaDSA/Farmacia/views.py
+++ farmaciaDSA/Farmacia/views.py
@@ -9,6 +9,20 @@ from Farmacia.forms import ClienteFormulario
 def homePage(request):
     return render(request, 'home.html', {})
 
+
+class registrarClientes():
+     def registroClientes(request):
+          cliente = ClienteFormulario()
+          return render(request, "clientes/registroClientes.html", {"form":cliente})
+     def procesarCliente(request):
+          cliente = ClienteFormulario(request.POST)
+          if cliente.is_valid():
+               cliente.save()
+               cliente = ClienteFormulario()
+
+          return render(request, "clientes/registroClientes.html", {"form":cliente, "mensaje:": 'Ok'})
+
+
 def registroClientes(request):
      cliente = ClienteFormulario()
      return render(request, "clientes/registroClientes.html", {"form":cliente})
