7b179d039eaaa21425a2d1add85c64fd68b55848
SirCarlosPedraza <59781461+CarlosPedraza456@users.noreply.github.com>
2023-07-25 03:00:54 -0400

    Clients Module: Finish

--- farmaciaDSA/Farmacia/urls.py
+++ farmaciaDSA/Farmacia/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.homePage, name='home'),
     path('clientes/', views.clientess, name='clientes'),
     path('registroCliente/', views.registroClientes, name='registroCliente'),
-    path('guardarCliente/', views.procesarCliente, name='guardarCliente')
+    path('guardarCliente/', views.procesarCliente, name='guardarCliente'),
+    path('editarCliente/<int:id_cliente>', views.editarClientes, name='editarCliente'),
+    path('actualizarCliente/<int:id_cliente>', views.actualizarCliente, name='actualizarCliente'),
+    path('eliminarCliente/<int:id_cliente>', views.eliminarCliente, name='eliminarCliente'),
 ]
--- farmaciaDSA/Farmacia/views.py
+++ farmaciaDSA/Farmacia/views.py
@@ -9,20 +9,6 @@ from Farmacia.models import clientes
 def homePage(request):
     return render(request, 'home.html', {})
 
-
-class registrarClientes():
-     def registroClientes(request):
-          cliente = ClienteFormulario()
-          return render(request, "clientes/registroClientes.html", {"form":cliente})
-     def procesarCliente(request):
-          cliente = ClienteFormulario(request.POST)
-          if cliente.is_valid():
-               cliente.save()
-               cliente = ClienteFormulario()
-
-          return render(request, "clientes/registroClientes.html", {"form":cliente, "mensaje:": 'Ok'})
-
-
 def registroClientes(request):
      cliente = ClienteFormulario()
      return render(request, "clientes/registroClientes.html", {"form":cliente})
@@ -33,14 +19,28 @@ def procesarCliente(request):
           cliente.save()
           cliente = ClienteFormulario()
 
-     return render(request, "clientes/registroClientes.html", {"form":cliente, "mensaje:": 'Ok'})
+     return render(request, "clientes/registroClientes.html", {"form":cliente, "mensaje": 'Ok'})
 
 
 def clientess(request):
      clients = clientes.objects.all()
      return render(request, 'clientes/clientes.html',{"clientes": clients})
 
-    
+def editarClientes(request, id_cliente):
+     cliente = clientes.objects.filter(id=id_cliente).first()
+     form = ClienteFormulario(instance=cliente)
+     return render(request, "clientes/editClientes.html", {"form": form,'cliente': cliente})
 
+def actualizarCliente(request, id_cliente):
+     cliente = clientes.objects.get(pk=id_cliente)
+     form = ClienteFormulario(request.POST, instance=cliente)
+     if form.is_valid():
+          form.save()
+     clientess = clientes.objects.all()
+     return render(request,'clientes/clientes.html', {"clientes": clientess} )
 
-
+def eliminarCliente(request, id_cliente):
+     cliente = clientes.objects.get(pk=id_cliente)
+     cliente.delete()
+     clientess = clientes.objects.all()
+     return render(request, "clientes/clientes.html", {"clientes": clientess, "mensaje": 'Ok'})
